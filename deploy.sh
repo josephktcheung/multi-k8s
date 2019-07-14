b8008cbf85c7fb036404913024f1e24e53fe1af6
Joseph Cheung <josephktcheung@gmail.com>
2019-07-15 02:17:28 +0800

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t josephktcheung/multi-client:latest -t josephktcheung/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t josephktcheung/multi-server:latest -t josephktcheung/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t josephktcheung/multi-worker:latest -t josephktcheung/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push josephktcheung/multi-client:latest
+docker push josephktcheung/multi-server:latest
+docker push josephktcheung/multi-worker:latest
+docker push josephktcheung/multi-client:$SHA
+docker push josephktcheung/multi-server:$SHA
+docker push josephktcheung/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=josephktcheung/multi-server:$SHA
+kubectl set image deployments/client-deployment server=josephktcheung/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=josephktcheung/multi-worker:$SHA
